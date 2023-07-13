22f5de49a7e18acc7f4872facb8de63d762aa532
Sasha Sorokin <10401817+Brawaru@users.noreply.github.com>
2023-07-13 13:51:16 +0200

    Some testing logging

--- server/storage/cached.ts
+++ server/storage/cached.ts
@@ -12,18 +12,27 @@ export default function cached(options: CachedOptions): Driver {
     ...driver,
     name: driver.name ? `cached:${driver.name}` : `cached`,
     options,
-    async hasItem(key) {
+    async hasItem(key, opts) {
+      console.log(`[cache] hasItem`, { key, opts });
       return (await memory.hasItem(key, {})) || (await driver.hasItem(key, {}));
     },
-    async getItem(key) {
+    async getItem(key, opts) {
+      console.log("[cache] getItem", { key, opts });
       const memoryLookup = await memory.getItem(key);
-      if (memoryLookup !== null) return memoryLookup;
+      if (memoryLookup !== null) {
+        console.log(`[cache] getItem: cache hit`, { key });
+        return memoryLookup;
+      }
 
+      console.log(`[cache] getItem: cache miss`, { key });
       const lookup = await driver.getItem(key);
+
       memory.setItem!(key, lookup as any, {});
+
       return lookup;
     },
-    async setItem(key, value) {
+    async setItem(key, value, opts) {
+      console.log(`[cache] setItem`, { key, value, opts });
       memory.setItem!(key, value, {});
       await driver.setItem?.(key, value, {});
     },
